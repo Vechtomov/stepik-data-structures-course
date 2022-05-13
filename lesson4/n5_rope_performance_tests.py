d58fb1ec23e75799e5a4cb63bc8b39683972736c
Vechtomov <qwedsacf@gmail.com>
2022-05-13 23:18:07 +0300

    lesson 4 task 5: tests

---
+++ lesson4/n5_rope_performance_tests.py
@@ -0,0 +1,24 @@
+from n5_rope import solve
+import random
+import cProfile
+import os
+from datetime import datetime
+
+
+def perf_test(m):
+    l = 100000
+    commands = []
+    for i in range(m):
+        beg = random.randint(0, l)
+        end = random.randint(beg, l)
+        pos = random.randint(0, l - (end - beg))
+        commands.append([beg, end, pos])
+
+    solve('a' * (l + 1), commands)
+ 
+
+if __name__ == "__main__":
+    random.seed(0)
+    file = f'rope_{datetime.now().strftime("%Y%m%d_%H%M%S")}.prof'
+    cProfile.run('perf_test(100)', file)
+    os.system(f'snakeviz {file}')
--- lesson4/n5_rope_tests.py
+++ lesson4/n5_rope_tests.py
@@ -1,4 +1,5 @@
-from n4_tree_no_annotations import Tree, Node, merge_trees, split_tree, solve, sum_segment, sum_segment_no_split
+from n5_rope import build_tree, Tree, Node, get_string, merge_trees, split_tree, permute, solve
+import string
 from typing import List, Tuple, Union, Callable
 import random
 
@@ -21,7 +22,7 @@ def in_order_keys(node: Union[Node, Tree]):
         node = node.root
     result = []
     for n in in_order(node):
-        result.append(n.k)
+        result.append(int(n.s))
     return result
 
 
@@ -33,8 +34,8 @@ def check_tree(t: Tree):
     def h(node: Node):
         return 0 if node is None else max(node.lh, node.rh) + 1
 
-    def s(node: Node):
-        return 0 if node is None else node.ls + node.rs + node.k
+    def c(node: Node):
+        return 0 if node is None else node.lc + node.rc + 1
 
     def check_node(node: Node):
         if node is None:
@@ -42,8 +43,8 @@ def check_tree(t: Tree):
         try:
             are_equal(h(node.l), node.lh)
             are_equal(h(node.r), node.rh)
-            are_equal(s(node.l), node.ls)
-            are_equal(s(node.r), node.rs)
+            are_equal(c(node.l), node.lc)
+            are_equal(c(node.r), node.rc)
             assert abs(node.lh - node.rh) <= 1, f"lh: {node.lh} rh: {node.rh}"
             check_node(node.l)
             check_node(node.r)
@@ -56,15 +57,18 @@ def check_tree(t: Tree):
 def test_tree(t: Tree, result: InOrderResult):
     check_tree(t)
     transform: Callable[[Node], NodeT] = lambda n: (
-        n.k, n.p.k if n.p else None)
+        int(n.s), int(n.p.s) if n.p else None)
     actual = list(map(transform, in_order(t.root)))
     are_equal(result, actual)
 
 
-def build_tree(nodes):
+def build_tree(nodes, to_right=True):
     t = Tree()
     for i in nodes:
-        t.add(i)
+        if to_right:
+            t.add_max(Node(str(i)))
+        else:
+            t.add_min(Node(str(i)))
     return t
 
 
@@ -72,18 +76,13 @@ def test_tree_add():
     def test(nodes, result: InOrderResult):
         test_tree(build_tree(nodes), result)
 
+    def test_left(nodes, result: InOrderResult):
+        test_tree(build_tree(nodes, to_right=False), result)
+
     test([1], [(1, None)])
     test([1, 2], [(1, None), (2, 1)])
-    test([2, 1], [(1, 2), (2, None)])
-    # right small rotation
     test([1, 2, 3], [(1, 2), (2, None), (3, 2)])
-    # left small rotation
-    test([3, 2, 1], [(1, 2), (2, None), (3, 2)])
-    # left big rotation
-    test([4, 5, 1, 0, 2, 3], [(0, 1), (1, 2), (2, None), (3, 4), (4, 2), (5, 4)])
-    # right big rotation
-    test([1, 0, 4, 5, 3, 2], [(0, 1), (1, 3), (2, 1), (3, None), (4, 3), (5, 4)])
-
+    test_left([3, 2, 1], [(1, 2), (2, None), (3, 2)])
 
 def test_tree_remove():
     def test(nodes: List[int], remove_node: int, result):
@@ -113,7 +112,7 @@ def test_tree_merge():
     test([1], [2], [(1, 2), (2, None)])
     test([1], [2, 3], [(1, 2), (2, None), (3, 2)])
     test([1, 2], [3, 4], [(1, 3), (2, 1), (3, None), (4, 3)])
-    test([1], [4, 5, 3, 2], [(1, 2), (2, 4), (3, 2), (4, None), (5, 4)])
+    test([1], [2,3,4,5,6], [(1, 2), (2, 3), (3, None), (4, 5), (5, 3), (6, 5)])
 
 
 def test_tree_split():
@@ -124,55 +123,66 @@ def test_tree_split():
         test_tree(t2, r2)
 
     test([1], 0, [], [(1, None)])
-    test([1], 1, [(1, None)], [])
+    test([1], 1, [], [(1, None)])
     test([1], 2, [(1, None)], [])
     test([1, 2], 0, [], [(1, 2), (2, None)])
+    test([1, 2], 1, [], [(1, 2), (2, None)])
+    test([1, 2], 2, [(1, None)], [(2, None)])
     test([1, 2], 3, [(1, 2), (2, None)], [])
-    test([1, 2], 1, [(1, None)], [(2, None)])
-    test([1, 3, 4], 2, [(1, None)], [(3, 4), (4, None)])
+    test([1, 2, 3], 0, [], [(1, 2), (2, None), (3, 2)])
 
 
 def test_solution():
-    def test(commands, expected):
-        actual = list(solve(map(lambda x: x.split(), commands)))
+    def test(s, commands, expected):
+        actual = solve(s, [map(int, x.split()) for x in commands])
         are_equal(expected, actual)
 
-    test(['+ 1'], [])
-    test(['- 1'], [])
-    test(['? 1'], ['Not found'])
-    test(['+ 1', '? 1'], ['Found'])
-    test(['+ 1', '- 1', '? 1'], ['Not found'])
-    test(['s 1 2'], ['0'])
-    test(['+ 1', 's 1 2'], ['1'])
-    test(['+ 1', '+ 2', 's 1 2'], ['3'])
-    test(['+ 1', '+ 2', 's 1 2', '+ 1', 's 1 2'], ['3', '4'])
-    test(['+ 1', '+ 2', 's 1 2', '+ 1', '? 1'], ['3', 'Found'])
+    test('a', ['0 0 0'], 'a')
+    test('ab', ['0 0 0'], 'ab')
+    test('ab', ['0 0 1'], 'ba')
+    test('ab', ['1 1 0'], 'ba')
+    test('ab', ['0 1 0'], 'ab')
+    test('hlelowrold', ['1 1 2', '6 6 7'], 'helloworld')
 
-    test(['+ 1_000_000_001', '? 0'], ['Found'])
-    test(['+ 1_000_000_002', '? 1'], ['Found'])
-    test(['+ 1_000_000_000', 's 0 1_000_000_000'], ['1000000000'])
-    test(['+ 1_000_000_000', 's 0 1_000_000_000', '? 0'], ['1000000000', 'Found'])
-    test(['+ 1', '+ 1_000_000_000', 's 0 1_000_000_000', '? 1'],
-         ['1000000001', 'Found'])
+def test_permute_random(n = 100):
+    def permute_baseline(s, beg, end, pos):
+        substr = s[beg:end+1]
+        s1 = s[:beg] + s[end+1:]
+        return s1[:pos] + substr + s1[pos:]
 
+    def test(s, beg, end, pos):
+        t = build_tree(list(s))
+        expected = permute_baseline(s, beg, end, pos)
+        t = permute(t, beg, end, pos)
+        are_equal(expected, get_string(t))
+
+    s = string.ascii_uppercase
+    l = len(s) - 1
+
+    for _ in range(n):
+        beg = random.randint(0, l)
+        end = random.randint(beg, l)
+        pos = random.randint(0, l - (end - beg))
+        test(s, beg, end, pos)
 
 def test_tree_random_add_remove(n=100):
     for i in range(n):
         nodes = list(range(100))
         random.shuffle(nodes)
-        keys = nodes.copy()
-        random.shuffle(keys)
+        node_to_remove = nodes.copy()
+        random.shuffle(node_to_remove)
         t = build_tree(nodes)
         check_tree(t)
         try:
-            for k in keys:
-                t.remove(k)
+            for k in node_to_remove:
+                node_list = list(in_order(t.root))
+                node_to_remove = random.choice(node_list)
+                t.remove(node_to_remove)
                 check_tree(t)
         except AssertionError as e:
             print(e)
-            print("Key:", k)
-            print(nodes)
-            print(keys)
+            print(node_list)
+            print(node_to_remove)
             raise
         assert t.root is None
 
@@ -184,21 +194,20 @@ def test_tree_random_split(n=100):
         keys = nodes.copy()
         random.shuffle(keys)
         try:
-            for k in keys:
+            for i in range(len(keys)):
                 t = build_tree(nodes)
                 t_keys = in_order_keys(t)
-                t1, t2 = split_tree(t, k)
+                t1, t2 = split_tree(t, i)
                 check_tree(t1)
                 check_tree(t2)
                 t1_keys = in_order_keys(t1)
                 t2_keys = in_order_keys(t2)
-                assert all(map(lambda x: x <= k, t1_keys)), f"{t1_keys} <= {k}"
-                assert all(map(lambda x: x > k, t2_keys)), f"{t2_keys} > {k}"
+                # assert all(map(lambda x: x <= k, t1_keys)), f"{t1_keys} <= {k}"
+                # assert all(map(lambda x: x > k, t2_keys)), f"{t2_keys} > {k}"
                 are_equal(set(t_keys), set(t1_keys + t2_keys))
                 are_equal(len(t_keys), len(t1_keys) + len(t2_keys))
         except AssertionError as e:
             print(e)
-            print("Key:", k)
             print(nodes)
             raise
 
@@ -231,74 +240,12 @@ def test_tree_random_merge(n=100):
             raise
 
 
-def test_tree_sum_segment():
-    def test(nodes, l, r, expected):
-        t = build_tree(nodes)
-        t_keys = in_order_keys(t)
-        s, tn = sum_segment_no_split(t, l, r)
-        check_tree(tn)
-        tn_keys = in_order_keys(tn)
-        are_equal(expected, s)
-        are_equal(t_keys, tn_keys)
-
-    test([], 0, 0, 0)
-    test([], 0, 1, 0)
-    test([], 1, 1, 0)
-    test([1], 0, 0, 0)
-    test([1], 0, 1, 1)
-    test([1], 1, 1, 1)
-    test([1], 1, 2, 1)
-    test([1], 2, 2, 0)
-    test([1], 2, 3, 0)
-    test([1, 2], 0, 0, 0)
-    test([1, 2], 0, 1, 1)
-    test([1, 2], 1, 1, 1)
-    test([1, 2], 1, 2, 3)
-    test([1, 2], 2, 2, 2)
-    test([1, 2], 2, 3, 2)
-    test([1, 2], 3, 3, 0)
-    test([1, 2, 3, 4], 1, 2, 3)
-    test([1, 2, 3, 4], 2, 3, 5)
-    test([1, 2, 3, 4], 3, 4, 7)
-    test([1, 2, 3, 4], 1, 3, 6)
-    test([1, 2, 3, 4], 2, 4, 9)
-    test([1, 2, 3, 4], 1, 4, 10)
-    test([3, 2, 4, 1, 5], 2, 4, 9)
-
-def test_tree_sum_segment_random(n=100):
-    for i in range(n):
-        arr = list(range(100))
-        random.shuffle(arr)
-        t = build_tree(arr)
-        t_keys = in_order_keys(t)
-        first = arr.copy()
-        second = arr.copy()
-        random.shuffle(first)
-        random.shuffle(second)
-        try:
-            for f, s in zip(first, second):
-                l, r = min(f, s), max(f, s)
-                sum_actual, t = sum_segment_no_split(t, l, r)
-                check_tree(t)
-                t_keys_new = in_order_keys(t)
-                are_equal(set(t_keys), set(t_keys_new))
-                sum_expected = sum([x for x in t_keys if l <= x <= r])
-                are_equal(sum_expected, sum_actual)
-        except AssertionError as e:
-            print(e)
-            print("l:", l, "r:", r)
-            print("Keys:", t_keys)
-            raise
-
-
 if __name__ == '__main__':
-    test_tree_add()
-    test_tree_remove()
-    test_tree_random_add_remove(10)
-    test_tree_merge()
-    test_tree_split()
-    test_tree_sum_segment()
-    test_tree_random_merge(10)
-    test_tree_random_split(10)
-    test_tree_sum_segment_random(10)
+    # test_tree_add()
+    # test_tree_random_add_remove(10)
+    # test_tree_merge()
+    # test_tree_split()
+    # test_tree_random_merge(10)
+    # test_tree_random_split(10)
     test_solution()
+    test_permute_random(100)
